7407bb7ffd8d12a2a8878a056392720a63ef1c59
raider001 <furnell.daniel@gmail.com>
2026-05-02 16:16:05 +0930

    feat: add @SuppressWarnings annotation to constructors for unused parameters

--- src/test/java/com/kalynx/lwdi/testclasses/MultiConstructorWithNoAnnotation.java
+++ src/test/java/com/kalynx/lwdi/testclasses/MultiConstructorWithNoAnnotation.java
@@ -2,10 +2,12 @@ package com.kalynx.lwdi.testclasses;
 
 public class MultiConstructorWithNoAnnotation {
 
+    @SuppressWarnings("unused")
     public MultiConstructorWithNoAnnotation(SimpleClassWithAnnotation ignored) {
 
     }
 
+    @SuppressWarnings("unused")
     public MultiConstructorWithNoAnnotation(SimpleClassWithoutAnnotation ignored) {
 
     }
